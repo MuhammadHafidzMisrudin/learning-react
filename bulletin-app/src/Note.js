25e9d2a7ba0f5b9dc00664d0728124209ecd5988
Hafidz <halfpheez92@gmail.com>
2018-05-20 23:32:57 +1000

    modified functionalities, rendering form and note and save.

--- bulletin-app/src/Note.js
+++ bulletin-app/src/Note.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
 import FaPencil from 'react-icons/lib/fa/pencil';
 import FaTrash from 'react-icons/lib/fa/trash';
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 // create a Note component.
 class Note extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false  // add edit button state.
+    }
+    this.edit = this.edit.bind(this); // set edit method in scope.
+    this.remove = this.remove.bind(this); // set remove method in scope.
+    this.save = this.save.bind(this); // set save method in scope.
+    this.renderForm = this.renderForm.bind(this); // set renderForm method in scope.
+    this.renderDisplay = this.renderDisplay.bind(this); // set renderDisplay method in scope.
+  }
   edit(){
-    alert("editing note");
+    //alert("editing note"); debug.
+    this.setState({
+      editing: true  // set the edit button state.
+    })
   }
   remove(){
     alert("removing note");
   }
-  render() {
+  save(){
+    alert("editing saved!");
+  }
+  renderForm() {
+    // create a form for a note to display.
+    return (
+      <div className="note">
+        <form>
+          <textarea/>
+          <button onClick={this.save}><FaFloppyO/></button>
+        </form>
+      </div>
+    )
+  }
+  renderDisplay() {
+    // create and render a note to display.
     return (
       <div className="note">
         <p>Learn React</p>
@@ -20,6 +50,13 @@ class Note extends Component {
         </span>
       </div>
     );
+  }
+  render(){
+    if (this.state.editing) {
+      return this.renderForm(); // if edit clicked, render a form.
+    } else {
+      return this.renderDisplay(); // otherwise, display a note.
+    }
   }
 }
 
